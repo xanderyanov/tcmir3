d12b27d2c87d7bf53245fed690dc1ab8352eebc0
xanderyanov <xanderyanov@gmail.com>
2020-05-21 12:08:11 +0400

    21-05-2020-12-07

---
+++ src/assets/js/main.js
@@ -0,0 +1,100 @@
+$(function () {
+  // главный слайдер на главной
+  if ($(".swiper-container1").length) {
+    var mySwiper1 = new Swiper(".swiper-container1", {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination1",
+        type: "bullets",
+        dynamicBullets: false,
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+      },
+      on: {
+        init: function () {
+          console.log("swiper initialized");
+          $(".swiper-slide").children(".swiper__cadr").removeClass("bounceInRight").fadeOut(500);
+
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("bounceInRight");
+          }, 500);
+        },
+        slideChange: function () {
+          $(".swiper-slide").children(".swiper__cadr").removeClass("bounceInRight").fadeOut(500);
+
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("bounceInRight");
+          }, 500);
+        },
+      },
+    });
+  }
+
+  //Слайдер на странице товара
+  if ($(".slider__new").length) {
+    var galleryThumbs = new Swiper(".slider__newThumbs", {
+      spaceBetween: 5,
+      loop: false,
+      direction: "vertical",
+      // Responsive breakpoints
+      breakpoints: {
+        // when window width is >= 320px
+        1: {
+          direction: "horizontal",
+          autoHeight: true,
+        },
+        // when window width is >= 480px
+        321: {
+          direction: "vertical",
+          autoHeight: false,
+        },
+      },
+      slidesPerView: 5,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: false,
+      autoHeight: false,
+    });
+    var galleryTop = new Swiper(".slider__newContent", {
+      spaceBetween: 0,
+      slidesPerView: 1,
+      loop: false,
+      effect: "slide",
+      autoHeight: false,
+      navigation: {
+        nextEl: ".sliderImgBtnNext",
+        prevEl: ".sliderImgBtnPrev",
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+      breakpoints: {
+        // when window width is >= 320px
+        1: {
+          autoHeight: true,
+        },
+        // when window width is >= 480px
+        321: {
+          autoHeight: false,
+        },
+      },
+      setWrapperSize: true,
+    });
+  }
+});
