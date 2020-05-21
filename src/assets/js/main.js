dc5692ef9c923cc5d95def8d186692de594d8605
xanderyanov <xanderyanov@gmail.com>
2020-05-21 18:57:09 +0400

    21-05-2020-18-56

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -97,4 +97,180 @@ $(function () {
       setWrapperSize: true,
     });
   }
+
+  $(".minus").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".plus").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+
+  $(".minus_tab").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 0 ? 0 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".plus_tab").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+
+  $(".content table").wrap('<div class="table_outer"></div>');
+
+  // Скрипт добавления в избранное через localstorage
+  var favorites = JSON.parse(localStorage.getItem("myFavorites")) || [];
+  if (favorites.length) {
+    console.log("есть записи");
+    $(".topFavBtn_notFav").hide();
+    $(".topFavBtn_yesFav").css("display", "flex");
+    // console.log(favorites);
+    favorites.forEach(function (entry) {
+      // console.log(entry);
+      $("#" + entry).addClass("inFav");
+      $("#" + entry)
+        .find(".tovarCard__fav")
+        .addClass("inFav");
+      $("#" + entry)
+        .find(".tovarCard__fav")
+        .children(".favOff")
+        .hide();
+      $("#" + entry)
+        .find(".tovarCard__fav")
+        .children(".favOn")
+        .show();
+
+      $(".topFavBtn").addClass("favLinkReady");
+    });
+  } else {
+    console.log("нет записей");
+  }
+  $(".tovarCard__fav").on("click", function (e) {
+    e.preventDefault();
+    var value = $(this).closest(".tovarCard__area").attr("id");
+
+    if ($(this).hasClass("inFav")) {
+      $(this).removeClass("inFav");
+      $(this).children(".favOff").show();
+      $(this).children(".favOn").hide();
+      // favorites = favorites.filter((val) => val !== "value");
+      var index = favorites.indexOf(value);
+      if (index >= 0) {
+        favorites.splice(index, 1);
+      }
+    } else {
+      $(this).addClass("inFav");
+      $(this).children(".favOff").hide();
+      $(this).children(".favOn").show();
+      favorites.push(value);
+    }
+
+    console.log(value);
+    console.log(favorites);
+
+    var serialFavorites = JSON.stringify(favorites);
+
+    localStorage.setItem("myFavorites", serialFavorites);
+
+    if (favorites.length) {
+      $(".topFavBtn_notFav").hide();
+      $(".topFavBtn_yesFav").css("display", "flex");
+    } else {
+      $(".topFavBtn_notFav").css("display", "flex");
+      $(".topFavBtn_yesFav").hide();
+    }
+  });
+
+  $(".topFavBtn_yesFav").click(function () {
+    console.log("переход на страницу избранного");
+    var parameters = favorites.join();
+    // console.log(favorites);
+    console.log(parameters);
+    $(location).attr("href", "http://mir.deltal.beget.tech/category.html/" + parameters);
+  });
+
+  $(".catalogFilter1__itemTitle").on("click", function () {
+    var fCont = $(this).closest(".catalogFilter1__item").find(".catalogFilter1__itemContent");
+    var fAll = $(this).closest(".catalogFilter1");
+    if ($(this).hasClass("open1")) {
+      $(this).removeClass("open1");
+      fAll.find(".catalogFilter1__itemContent").removeClass("visible");
+    } else {
+      fAll.find(".catalogFilter1__itemTitle").removeClass("open1");
+      fAll.find(".catalogFilter1__itemContent").removeClass("visible");
+      $(this).addClass("open1");
+      fCont.addClass("visible");
+    }
+  });
+
+  // if ($(".filterBox__checkbox").length) {
+  //   $(".filterEnterBtn").on("click", function () {
+  //     var filtrItemParent = $(this).closest(".filterBox");
+
+  //     if (filtrItemParent.find(".xcheckbox input").is(":checked")) {
+  //       console.log("Выбрано");
+  //       filtrItemParent.closest(".catalogFilter1__item").find(".catalogFilter1__itemTitle").addClass("active");
+  //       $(".catalogFilter1__itemContent").removeClass("visible");
+  //       $(".catalogFilter1__itemTitle").removeClass("open1");
+  //     } else {
+  //       console.log("не выбрано");
+  //       filtrItemParent.closest(".catalogFilter1__item").find(".catalogFilter1__itemTitle").removeClass("active");
+  //       $(".catalogFilter1__itemContent").removeClass("visible");
+  //       $(".catalogFilter1__itemTitle").removeClass("open1");
+  //     }
+  //   });
+  // }
+
+  $(".filterEnterBtn").on("click", function () {
+    var filtrItemParent = $(this).closest(".filterBox");
+
+    if (filtrItemParent.find("input").is(":checked")) {
+      console.log("Выбрано");
+      filtrItemParent.closest(".catalogFilter1__item").find(".catalogFilter1__itemTitle").addClass("active");
+      $(".catalogFilter1__itemContent").removeClass("visible");
+      $(".catalogFilter1__itemTitle").removeClass("open1");
+    } else {
+      console.log("не выбрано");
+      filtrItemParent.closest(".catalogFilter1__item").find(".catalogFilter1__itemTitle").removeClass("active");
+      $(".catalogFilter1__itemContent").removeClass("visible");
+      $(".catalogFilter1__itemTitle").removeClass("open1");
+    }
+  });
+
+  if ($(".filter__checkboxColor").length) {
+    $(".checkboxColor__item").click(function (e) {
+      e.preventDefault();
+      if ($(this).hasClass("checked")) {
+        $(this).removeClass("checked");
+        $(this).children(".checkboxColorBox").removeClass("checked");
+        $(this).children(".checkboxColorTitle").removeClass("checkColor");
+        $(this).children("input").removeAttr("checked");
+      } else {
+        $(this).addClass("checked");
+        $(this).children(".checkboxColorBox").addClass("checked");
+        $(this).children(".checkboxColorTitle").addClass("checkColor");
+        $(this).children("input").attr("checked", "checked");
+      }
+    });
+    $(".filter__checkboxColorReset").click(function (e) {
+      console.log("сброс фильтров");
+      e.preventDefault();
+      $(this).closest(".filterBox").find("input[type=checkbox]").removeAttr("checked");
+      $(this).closest(".filterBox").find(".checkboxColor__item").removeClass("checked");
+      $(this).closest(".filterBox").find(".checkboxColorBox").removeClass("checked");
+      $(this).closest(".filterBox").find(".checkboxColorTitle").removeClass("checkColor");
+    });
+  }
 });
